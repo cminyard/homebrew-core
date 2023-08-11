5ebd02e6ba000006947d48e6961febe945f88457
Corey Minyard <minyard@acm.org>
2023-08-11 13:26:47 -0500

    Update to gensio 2.7.2 and fix some issues
    Add :build to the dependencies that are build-only
    
    Set the proper sysconfig location to /opt/homebrew/etc and clean up
    the messy config line.
    
    Add a service for gtlsshd.
    
    Signed-off-by: Corey Minyard <minyard@acm.org>

--- Formula/gensio.rb
+++ Formula/gensio.rb
@@ -1,16 +1,16 @@
 class Gensio < Formula
   desc "Stream I/O Library"
   homepage "https://github.com/cminyard/gensio"
-  url "https://downloads.sourceforge.net/project/ser2net/ser2net/gensio-2.7.1.tar.gz"
-  sha256 "e096158b52ef5d726d072e1fb68b481292d304d45a48b48109781e9a5545eafd"
+  url "https://downloads.sourceforge.net/project/ser2net/ser2net/gensio-2.7.2.tar.gz"
+  sha256 "4fca20de5b5e5ff066a65025d29cc85427a47e5e0605d49f67fef03520fbf1e2"
   license all_of: ["LGPL-2.1-only", "GPL-2.0-only", "Apache-2.0"]
 
+  depends_on "go" => [:build]
+  depends_on "pkg-config" => [:build]
+  depends_on "swig" => [:build]
   depends_on "glib"
-  depends_on "go"
   depends_on "openssl@3"
-  depends_on "pkg-config"
   depends_on "python@3.11"
-  depends_on "swig"
 
   def python3
     "python3.11"
@@ -19,10 +19,18 @@ class Gensio < Formula
   def install
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     system "./configure", *std_configure_args.reject { |s| s["--disable-debug"] },
-       "--disable-silent-rules", "--with-pythoninstall=#{lib}/gensio-python",
-       "--with-link-ssl-with-main"
+                          "--disable-silent-rules",
+                          "--with-pythoninstall=#{lib}/gensio-python",
+                          "--with-link-ssl-with-main",
+                          "--sysconfdir=#{etc}"
     system "make", "install"
     (prefix/Language::Python.site_packages(python3)).install_symlink Dir["#{lib}/gensio-python/*"]
+  end
+
+  service do
+    run [opt_sbin/"gtlsshd", "--nodaemon", "--pam-service", "sshd"]
+    keep_alive true
+    working_dir HOMEBREW_PREFIX
   end
 
   test do
