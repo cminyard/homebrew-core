d8e1598dfe0702fc6c8e1c995e304506e686b24d
Corey Minyard <minyard@acm.org>
2023-08-18 15:01:40 -0500

    gensio: Update to 2.7.3
    This fixes some more MacOS issues.
    
    Signed-off-by: Corey Minyard <minyard@acm.org>

--- Formula/gensio.rb
+++ Formula/gensio.rb
@@ -1,8 +1,8 @@
 class Gensio < Formula
   desc "Stream I/O Library"
   homepage "https://github.com/cminyard/gensio"
-  url "https://downloads.sourceforge.net/project/ser2net/ser2net/gensio-2.7.2.tar.gz"
-  sha256 "4fca20de5b5e5ff066a65025d29cc85427a47e5e0605d49f67fef03520fbf1e2"
+  url "https://downloads.sourceforge.net/project/ser2net/ser2net/gensio-2.7.3.tar.gz"
+  sha256 "43cf2af7e9c06b963b4edf0b5b066ac2a248c0d796399d19bd24e28d3292ce8d"
   license all_of: ["LGPL-2.1-only", "GPL-2.0-only", "Apache-2.0"]
 
   depends_on "go" => [:build]
@@ -10,6 +10,7 @@ class Gensio < Formula
   depends_on "swig" => [:build]
   depends_on "glib"
   depends_on "openssl@3"
+  depends_on "portaudio"
   depends_on "python@3.11"
 
   def python3
@@ -22,6 +23,7 @@ class Gensio < Formula
                           "--disable-silent-rules",
                           "--with-pythoninstall=#{lib}/gensio-python",
                           "--with-link-ssl-with-main",
+                          "--with-homebrew-prefix=#{HOMEBREW_PREFIX}",
                           "--sysconfdir=#{etc}"
     system "make", "install"
     (prefix/Language::Python.site_packages(python3)).install_symlink Dir["#{lib}/gensio-python/*"]
@@ -30,6 +32,7 @@ class Gensio < Formula
   service do
     run [opt_sbin/"gtlsshd", "--nodaemon", "--pam-service", "sshd"]
     keep_alive true
+    require_root true
     working_dir HOMEBREW_PREFIX
   end
 
